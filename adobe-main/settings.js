ef9391a36646edba6adc1bbe40709055a3fa9b8a
brede22 <brede@viessmann-schule.de>
2025-03-12 12:48:05 -0700

    Update settings.js

--- adobe-main/settings.js
+++ adobe-main/settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-	botToken: "5433611121:AAFMpeQpC5y_y0PveL5sd77QQIXHuz6TOr4",
-	chatId: "5219819186",
+	botToken: "8108867887:AAFwibT5stSPbbivvn4acYKIcimucbU95JQ",
+	chatId: "6183121912",
 };
